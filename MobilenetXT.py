bce8a58a85676db24c122357cf48b190830662a0
carolchenyx <carolchenyx@gmail.com>
2020-10-30 12:01:14 +0800

    modify some laters

--- MobilenetXT.py
+++ MobilenetXT.py
@@ -34,7 +34,7 @@ class PoseNetNX:
             [6, 384, 4, 1],
             [6, 576, 4, 2],
             [6, 960, 2, 1],
-            [6, self.last_channel / width_mult, 1, 1]
+            [6, 1280, 1, 1]
         ]
         self.bottleneck_type = 0
         for t, c, n, s in sand_glass_setting:
@@ -48,28 +48,9 @@ class PoseNetNX:
                 layerDescription = "l" + str(self.bottleneck_type) + "-bottleneck-n" + str(i+1)
                 output = lProvider.inverted_bottleneck(output, t, output_channel, stride, k_s=3, dilation=1, scope=layerDescription)
 
-        # lProvider1 = LayerProvider(self.transtrain)
-        # inverted_residual_setting1 = [
-        #     # t, c, n, s
-        #     [6, 320, 1, 1]
-        # ]
-        # for t, c, n, s in inverted_residual_setting1:
-        #     self.bottleneck_type += 1
-        #     output_channel = adaptChannels(c)
-        #     for i in range(n):
-        #         if i == 0:
-        #             stride = s
-        #         else:
-        #             stride = 1
-        #         layerDescription = "l" + str(self.bottleneck_type) + "-bottleneck-n" + str(i+1)
-        #         output = lProvider1.inverted_bottleneck(output, t, output_channel, stride, k_s=3, dilation=1, scope=layerDescription)
 
-
-        #for DUC
-        self.output = outputlayer.fornetworks_DUC(output,totalJoints)
-        #for no DUC
-        # output = lProvider.convb(output, 1, 1, adaptChannels(1280), 1, "2-conv-1280-2-1", relu=True)
-        # self.output = outputlayer.fornetworks(output, totalJoints)
+        output = lProvider.convb(output, 1, 1, adaptChannels(1280), 1, "2-conv-1280-2-1", relu=True)
+        self.output = outputlayer.fornetworks(output, totalJoints)
 
 
     def _make_divisible(v, divisor, min_value=None):
